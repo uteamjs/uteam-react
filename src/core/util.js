dd1db0e8ba43db5fdef1d4a9bb9680f89999e533
sysadmin <ddrryyll@gmail.com>
2022-10-28 20:35:00 +0800

    minor fix on util.js

--- src/core/util.js
+++ src/core/util.js
@@ -113,7 +113,7 @@ export function merge(target, ...sources) {
  */
 export function getParameterByName(name, url = window.location.href) {
     name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
         results = regex.exec(url);
     if (!results) return null;
     if (!results[2]) return '';
